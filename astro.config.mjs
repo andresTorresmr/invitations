241859c27c057e2ea2a6478d13e07343c8b6bc6c
andresTorresmr <andrestorresm001@gmail.com>
2024-09-14 21:55:51 -0600

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,30 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: "[name]-[hash].js",
+        },
+      },
+    },
+  },
+  security: {
+    cors: {
+      allowed_origins: ["*"],
+      allowed_methods: ["*"],
+      allowed_headers: [
+        "X-CSRF-Token",
+        "X-Requested-With",
+        "Accept, Accept-Version",
+        "Content-Length",
+        "Content-MD5",
+        "Content-Type",
+        "Date",
+        "X-Api-Version",
+      ],
+      // more relevant options
+    },
+  },
 });
